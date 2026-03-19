b64f46d62871351477b7672905991bd96f526a92
Dennis_koeklue_Linuxmint <dennis.koeklue@gmail.com>
2026-03-19 11:16:37 +0100

    New version data 19-03-2026

---
+++ summary.js
@@ -0,0 +1,11 @@
+// Global Variable
+
+let toDoNumbers = document.getElementById(`toDoNumbers`)
+let doneNumbers = document.getElementById(`doneNumbers`)
+let urgentNumbers = document.getElementById(`urgentNumbers`)
+let dueDate = document.getElementById(`dueDate`)
+let totalTasksNumbers = document.getElementById(`totalTasksNumbers`)
+let inProgressNumber = document.getElementById(`inProgressNumber`)
+let awaitingFeedbackNumber = document.getElementById(`awaitingFeedbackNumber`)
+
+
---
+++ test-databank.js
@@ -0,0 +1,18 @@
+const todos = [
+  { id: 1, status: "todo", urgent: true, date: "2026-03-20" },
+  { id: 2, status: "done", urgent: false, date: null },
+  { id: 3, status: "inProgress", urgent: true, date: null },
+  { id: 4, status: "feedback", urgent: false, date: "2026-03-22" },
+  { id: 5, status: "todo", urgent: false, date: null},
+  { id: 6, status: "done", urgent: false, date: null},
+  { id: 7, status: "inProgress", urgent: true, date: null},
+  { id: 8, status: "todo", urgent: true, date: "2026-03-20" },
+  { id: 9, status: "done", urgent: false, date: null },
+  { id: 10, status: "inProgress", urgent: true, date: null },
+  { id: 11, status: "feedback", urgent: false, date: "2026-03-22" },
+  { id: 12, status: "todo", urgent: false, date: null},
+  { id: 13, status: "done", urgent: false, date: null},
+  { id: 14, status: "inProgress", urgent: true, date: null},
+  
+
+];
