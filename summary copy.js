a636a3c0625d97b3fe78f4747c89da54a978526e
Dennis_koeklue_Linuxmint <dennis.koeklue@gmail.com>
2026-03-19 11:13:14 +0100

    test delet

--- summary copy.js
+++
@@ -1,7 +0,0 @@
-let DropDowncontain = document.getElementById(`dropdown-menu`)
-let dropdownButton = document.getElementById(`header-button`)
-
-function DropDownOnClick() {
-    
-    DropDowncontain.classList.toggle(`d_none`)
-}
