02c3469e910fe4bcb507a41ed89b2ff26c964940
Dennis_koeklue_Linuxmint <dennis.koeklue@gmail.com>
2026-04-09 10:27:59 +0200

    all-in-one changes on contacts site

--- contacts-js.js
+++ contacts-js.js
@@ -1,3 +1,5 @@
+// all Global Variablen
+
 let addContactButton = document.getElementById(`add-contact`)
 let contactListDiv = document.getElementById(`contact-list`)
 let contactBadge = document.getElementById(`contact-badge`)
@@ -13,3 +15,39 @@ let contactDetailsDiv = document.getElementById(`contact-details`)
 let spanEmail = document.getElementById(`span-email`)
 let spanPhone = document.getElementById(`span-phone`)
 let contactPopUpAdd = document.getElementById(`contact-pop-add`)
+
+let Firebase_URL = "./contacts.json"
+
+
+
+
+
+
+async function loadingContacs() {
+    
+    try{
+        const response = await fetch(Firebase_URL)
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const ResponseToJson = await response.json();
+
+        let fetchdData = {};
+        
+        if (ResponseToJson && typeof ResponseToJson === "object") {
+      for (const [id, todoData] of Object.entries(ResponseToJson)) {
+        fetchdData[id] = { id, ...todoData };
+      }
+    }
+
+        console.log("fetchData:", fetchdData);
+        return fetchdData;
+    }catch (error) {
+    console.error("Error loading todos:", error);
+    return {};
+  }
+
+
+}
