4e16e8c3fac7561d3a5a3cd0a2db12de5f518804
Dennis_koeklue_Linuxmint <dennis.koeklue@gmail.com>
2026-06-15 12:17:49 +0200

    changes on js Sidebar and header

--- Js/Sidebar-and-header-script.js
+++ Js/Sidebar-and-header-script.js
@@ -2,7 +2,7 @@ let DropDowncontain = document.getElementById(`dropdown-menu`)
 let dropdownButton = document.getElementById(`header-button`)
 const mediaQuery = window.matchMedia("(max-width: 1092px)");
 //später entfernen
-localStorage.setItem("username", "Dennis Kollak");
+// localStorage.setItem("username", "Dennis Kollak");
 
 
 DropDowncontain.classList.add("d_none");
