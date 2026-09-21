5b32717c1e151c3648cfc1d3635b7a2594dee8bf
Stefan Weidner <stephan721@gmx.net>
2026-09-21 11:43:51 +0200

    authscript exceptions included

--- Js/authScript.js
+++ Js/authScript.js
@@ -1,10 +1,16 @@
 window.addEventListener("load", function () {
+  const currentPage = window.location.pathname.split("/").pop();
+  const publicPages = ["privacy.html", "legalnotes.html"];
+
+  if (publicPages.includes(currentPage)) {
+    return;
+  }
+
   const currentUser = localStorage.getItem("currentUser");
 
   if (!currentUser) {
     window.location.href = "./index.html";
   }
 });
-
 
 
